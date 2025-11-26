789ce78d8acfb19e4b060904ad241069966968d1
Sukhpreet Singh <>
2025-11-26 18:19:24 +0530

    create express server and try to listen on some paths

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
-console.log("initial commit")
+const express = require("express");
+
+const app = express();
+
+app.listen(7777, () => {
+    console.log("the express server started successfully");
+});
+
+app.use("/test", (req, res) => {
+ res.send("Hello from the server!!");
+});
+
+
+app.use("/namaste", (req, res) => {
+ res.send("namaste!!!");
+});
+
+
+
