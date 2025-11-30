1fbe06ffc71092554b7254945fc55b6ae9980aee
Sukhpreet Singh <>
2025-11-30 19:44:59 +0530

    explore regex routing, query params and dynamic routes

--- src/app.js
+++ src/app.js
@@ -6,15 +6,23 @@ app.listen(7777, () => {
     console.log("the express server started successfully");
 });
 
+// routes are basically regexes, you can also write regexes here instead of path strings
+
 app.use("/test", (req, res) => {
  res.send("Hello from the server!!");
 });
 
+// this is how you handle the query params /user?userid=10&password=12345
 app.get("/user",(req, res) => {
+ console.log(req.query["userid"])
+console.log(req.query["password"])
+
  res.send({firstname: "sukhpreet", lastname: "singh"});
 });
 
-app.post("/user", (req, res) => {
+// how make the url itself dynamic, you can use ':' and get them from req.params
+app.post("/user/:userid/:name/:password", (req, res) => {
+    console.log(req.params)
  res.send("user data saved successfully to the database!");
 });
 
