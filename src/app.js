c0f3ae26cca44f386ff46f1300b8d90b0e4df907
Sukhpreet Singh <>
2025-11-30 19:28:28 +0530

    test the user routes with different http methods

--- src/app.js
+++ src/app.js
@@ -10,10 +10,17 @@ app.use("/test", (req, res) => {
  res.send("Hello from the server!!");
 });
 
-
-app.use("/namaste", (req, res) => {
- res.send("namaste!!!");
+app.get("/user",(req, res) => {
+ res.send({firstname: "sukhpreet", lastname: "singh"});
 });
 
+app.post("/user", (req, res) => {
+ res.send("user data saved successfully to the database!");
+});
+
+
+app.delete("/user", (req, res) => {
+ res.send("user data deleted successfully from the database!");
+});
 
 
