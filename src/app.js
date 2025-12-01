482312c696d223a43816fe854e1339b3e61c74b7
Sukhpreet Singh <>
2025-12-01 18:27:46 +0530

    exploring route handlers

--- src/app.js
+++ src/app.js
@@ -12,13 +12,24 @@ app.use("/test", (req, res) => {
  res.send("Hello from the server!!");
 });
 
-// this is how you handle the query params /user?userid=10&password=12345
-app.get("/user",(req, res) => {
- console.log(req.query["userid"])
-console.log(req.query["password"])
+// you can pass as many route handlers in the function arguments as you want
+// to call the next handler just using "next()" function. 
+// you can also add them in an array here like app.get("/user", rh1, rh2, [rh3, rh4], rh5)
+// just remember to call at the right place s
+app.get("/user",
+    (req, res, next) => {
 
+ //res.send({firstname: "sukhpreet", lastname: "singh"})
+ console.log("this is the first route handler");
+ next();
+},
+
+    (req, res) => {
  res.send({firstname: "sukhpreet", lastname: "singh"});
-});
+}
+
+
+ );
 
 // how make the url itself dynamic, you can use ':' and get them from req.params
 app.post("/user/:userid/:name/:password", (req, res) => {
