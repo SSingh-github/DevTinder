f9cb6d6b93ad327e090dcee62eb67256eec0a89a
Sukhpreet Singh <ss0218308@gmail.com>
2026-01-12 16:55:55 +0530

    attach the user in req body in user auth middleware.

--- src/app.js
+++ src/app.js
@@ -67,20 +67,8 @@ app.post("/login", async (req, res) => {
 app.get("/profile", userAuth, async (req, res) => {
 
     try {
-    //     const {token} = req.cookies;
-    //     if(!token) {
-    //         throw new Error("invalid token");
-    //     }
-    //     console.log(token);
-    //     const {_id} = await jwt.verify(token, "DEVTINDER");
-        
-    //     const user = await User.findById(_id);
-    //     if(!user) {
-    //         throw new Error("invalid token");
-    //     }
-
-    //    res.send(user);
-    res.send("user found success");
+        const user = req.user;
+        res.send(user);
     } catch (err) {
         res.status(400).send("Error: " + err);
         console.log("error while saving the user")
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -16,7 +16,7 @@ const userAuth = async (req, res, next) => {
         if(!user) {
             throw new Error("user not found");
         }
-
+        req.user = user;
         next();
     } catch (error) {
         res.status(400).send("Error: " + error.message)
