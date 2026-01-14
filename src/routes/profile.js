fbd106b8069bade0a60122d8245d9853b1026500
Sukhpreet Singh <ss0218308@gmail.com>
2026-01-14 16:45:07 +0530

    add the profile view and profile edit apis

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const profileRouter = express.Router();
 const {userAuth} = require("../middlewares/auth");
+const {validateUserPatchData} = require("../Utils/validation");
+const {User} = require("../models/user");
 
 
-profileRouter.get("/profile", userAuth, async (req, res) => {
+profileRouter.get("/profile/view", userAuth, async (req, res) => {
 
     try {
         const user = req.user;
@@ -13,6 +15,19 @@ profileRouter.get("/profile", userAuth, async (req, res) => {
         console.log("error while saving the user")
     }
 
+});
+
+profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
+
+    const {_id} = req.user;
+    validateUserPatchData(req);
+    try {
+        const user = await User.findByIdAndUpdate(_id, req.body);
+        res.send("user updated successfully");
+    } catch (err) {
+        res.status(400).send("Error: " + err);
+        console.log("error while updating the user")
+    }
 });
 
 module.exports = profileRouter;
